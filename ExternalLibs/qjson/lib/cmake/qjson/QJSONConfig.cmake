6968b618b32184ec56316d267cadfe4fa75d1183
Rui Sebastiao <ruisebastiao@gmail.com>
2014-08-22 23:35:59 +0100

    Revision 2.1.2

--- ExternalLibs/qjson/lib/cmake/qjson/QJSONConfig.cmake
+++
@@ -1,6 +0,0 @@
-GET_FILENAME_COMPONENT(myDir ${CMAKE_CURRENT_LIST_FILE} PATH)
-
-SET(QJSON_LIBRARIES qjson)
-SET(QJSON_INCLUDE_DIR "E:/Projectos/Programming/qt/qjson-master/build/_preferred_path_/include")
-
-include(${myDir}/QJSONTargets.cmake)
